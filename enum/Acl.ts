8424b81dbd3b8ad3e82ac25c8d7ff50600923249
Linus Decker Jr <wizer0x64@Wizer0x64Pro.local>
2017-10-04 09:55:39 -0400

    updating models

--- enum/Acl.ts
+++ enum/Acl.ts
@@ -1,1 +1,23 @@
 export const enum AclPolicy {Allow = 1, Deny}
+
+export interface IAclAction {
+    All: string;
+    Read: string;
+    Add: string;
+    Edit: string;
+    Delete: string;
+    Login: string;
+    Register: string;
+    Forget: string;
+}
+
+export const AclAction: IAclAction = {
+    All: '*',
+    Read: 'read',
+    Add: 'add',
+    Edit: 'edit',
+    Delete: 'del',
+    Login: 'login',
+    Register: 'register',
+    Forget: 'forget'
+};
